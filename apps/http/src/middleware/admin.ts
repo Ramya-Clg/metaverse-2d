487dc960b20c720fac8ad43692167b502ae1f363
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-09 22:09:41 +0530

    metadata

---
+++ apps/http/src/middleware/admin.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+const secret = process.env.JWT_SECRET;
+
+export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers['authorization'];
+    const token = header?.split(' ')[1];
+    if (!token) {
+        res.status(403).json({
+            msg: 'Unauthorized',
+        });
+        return;
+    }
+    try {
+        const decoded = jwt.verify(token, secret!) as { role: string, userId: string };
+        if (decoded.role != 'Admin') {
+            res.status(403).json({
+                msg: 'Unauthorized',
+            });
+            return;
+        }
+        req.role = decoded.role;
+        req.userId = decoded.userId;
+        next(); 
+    } catch (e) {
+        res.status(401).json({
+            msg: 'Unauthorized',
+        });
+        return;
+    }
+};
--- apps/http/src/routes/v1/user.ts
+++ apps/http/src/routes/v1/user.ts
@@ -1,10 +1,28 @@
 import { Router } from 'express';
+import { UpdateMetadataSchema } from '../../types';
 export const userRouter = Router();
+import client from '@repo/db/client'; 
+import { userMiddleware } from '../../middleware/user';
 
-userRouter.post('/metadata',(req,res)=>{
-
+userRouter.post('/metadata',userMiddleware, async (req,res)=>{
+    const parsedData = UpdateMetadataSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({
+            msg: 'Validation failed',
+        });
+        return;
+    }
+    await client.user.update({
+        where:{
+            id: req.userId
+        },
+        data:{
+            avatarId: parsedData.data.avatarId
+        }
+    })
+    res.json({msg:"metadata Updated"})
 })
 
 userRouter.get('/metadata/bulk',(req,res)=>{
-
+ const parsedDate = 
 })
